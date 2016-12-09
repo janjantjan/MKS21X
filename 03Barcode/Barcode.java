fe68cd40760ae1152d083af6cf65eecbb30ea2b2
Janjan Tjan <Janjan Tjan>
2016-12-08 19:57:16 -0500

    Updated Version: Compiles

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -14,7 +14,7 @@ public class Barcode implements Comparable<Barcode>{
 
     public Barcode(String zip) {	
 	if (zip.length() > 10) {
-	    throw new UnsupportedOperationException("Limit Ten Digits");
+	    throw new IllegalArgumentException("Limit Ten Digits");
 	}
 	_zip = zip;
 
@@ -42,7 +42,7 @@ public class Barcode implements Comparable<Barcode>{
 	return sum;
 	    }
     
-    private String bars (int num) {
+    private String code (int num) {
 	String code = "";
 	if (num == 1) {
 	    code = ":::||" ;}
@@ -65,21 +65,66 @@ public class Barcode implements Comparable<Barcode>{
 	if (num == 0) {
 	    code = "||:::" ;}
 	return code;
-    }   
+    }
+
+    private String decode (String code) {
+	String num = "";
+	for (int i = 0; i < code.length(); i++){
+	    if (!((code.charAt(i)!= ':') || (code.charAt(i)!= '|'))){
+		throw new IllegalArgumentException("Invalid characters");
+	    }
+	}
+	if (code == ":::||") {
+	    num += 1 ;}
+	if (code == "::|:|") {
+	    num += 2 ;}
+	if (code == "::||:") {
+	    num += 3 ;}
+	if (code == ":|::|") {
+	    num += 4 ;}
+	if (code == ":|:|:") {
+	    num += 5 ;}
+	if (code == ":||::") {
+	    num += 6 ;}
+	if (code == "|:::|") {
+	    num += 7 ;}
+	if (code == "|::|:" ) {
+	    num += 8 ;}
+	if (code == "|:|::") {
+	    num += 9 ;}
+	if (code == "||:::") {
+	    num += 0 ;}
+	else {throw new IllegalArgumentException("Unidentified Code:" + code);}
+	return num;
+    }
 
    
     public String toString(){
 	String fin = "|";
 	for (int dig = 0; dig < _zip.length(); dig++)
 	    {
-		fin += bars(extract(_zip, dig));
+		fin += code(extract(_zip, dig));
 	    }
 	fin += "|";
 	return fin;
     }
 	
-    
-    //Add toZip
+    public String toZip(String code){
+	String zip = "";
+	if (0 != ((code.length()-2)%5)) {
+	    throw new IllegalArgumentException ("Illegal length");}
+	for (int bars = 1; bars < code.length(); bars++){
+	    String seg = code.substring(bars+3);
+	    zip += decode(seg);
+	}
+	
+	int last = extract (zip , (zip.length()-1));
+	if (last != _checkDigit) {
+	    throw new IllegalArgumentException("Invalid checkdigit");
+	}
+	return zip;		
+		
+    }
 
 
     public int compareTo(Barcode other){
