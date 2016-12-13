470d6f8cc7e84517cf85ba61d3380aa841fb41bf
janjantjan <janjan.tjan@gmail.com>
2016-12-13 06:28:47 -0500

    V1:Not tested

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -4,10 +4,7 @@ public class Sorts{
     return "01.Tjan.Janice"; 
   }
 
-  /**Selection sort of an int array. 
-  *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
-  */
+  
   public static void selectionSort(int[] data){ 
       for (int i = 0; i < data.size(); i++){
 	  int[] newdata;
