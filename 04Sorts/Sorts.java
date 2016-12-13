a2c6b1894314407bf33ecc71adc6cda0bf9df685
janjantjan <janjan.tjan@gmail.com>
2016-12-13 06:34:38 -0500

    V2: Compiles!

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -6,24 +6,31 @@ public class Sorts{
 
   
   public static void selectionSort(int[] data){ 
-      for (int i = 0; i < data.size(); i++){
-	  int[] newdata;
+      for (int i = 0; i < data.length; i++){
+	  int[] newdata = new int[data.length];
 	  int smallest = data[i];
-	  for (int j = 0; j < data.size(); j++){
+	  for (int j = 0; j < data.length; j++){
 	      if (data[i+1]<data[i]){
 		  smallest = data [i+1];
 	      }
 	  }
 	  newdata[i]= smallest;
       }
-      return newdata;
   }
 
+    // public static void main(String[] args) {
+    //       int [] A = new int[5];
+    //       A[0]=9;
+    // A[0]=9;
+    // A[0]=9;
+    // A[0]=9;
+    // A[0]=9;
+    // A[0]=9;
 
 
-
-
- }
 
 }
+
+
+
 
