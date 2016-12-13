4486476a42e8fd64e4bbb015912c5560f73d1983
janjantjan <janjan.tjan@gmail.com>
2016-12-13 06:38:46 -0500

    V3: REDO at home

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -6,26 +6,32 @@ public class Sorts{
 
   
   public static void selectionSort(int[] data){ 
-      for (int i = 0; i < data.length; i++){
-	  int[] newdata = new int[data.length];
+      for (int i = 0; i < data.length; i++){          
 	  int smallest = data[i];
 	  for (int j = 0; j < data.length; j++){
 	      if (data[i+1]<data[i]){
 		  smallest = data [i+1];
+data 
 	      }
 	  }
 	  newdata[i]= smallest;
       }
   }
 
-    // public static void main(String[] args) {
-    //       int [] A = new int[5];
-    //       A[0]=9;
-    // A[0]=9;
-    // A[0]=9;
-    // A[0]=9;
-    // A[0]=9;
-    // A[0]=9;
+    public static String toString (int[] data) {
+
+
+
+    public static void main(String[] args) {
+	int [] A = new int[5];
+	A[0]=9;
+	A[1]=4;
+	A[2]=5;
+	A[3]=3;
+	A[4]=7;
+	selectionsort(A);
+	System.out.println (
+
 
 
 
